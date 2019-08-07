872d01ac0111a68fe32f3de99ef5147e30711f44
TheMode <themode@outlook.fr>
2019-08-07 04:05:35 +0200

    First commit

---
+++ src/main/java/fr/minestom/api/Minestom.java
@@ -0,0 +1,4 @@
+package fr.minestom.api;
+
+public class Minestom {
+}
---
+++ src/main/java/fr/minestom/api/MinestomServer.java
@@ -0,0 +1,19 @@
+package fr.minestom.api;
+
+public class MinestomServer {
+
+    private int port;
+
+    public MinestomServer(int port) {
+        this.port = port;
+        // TODO initialize Minestom
+    }
+
+    public void start(int port) {
+
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
