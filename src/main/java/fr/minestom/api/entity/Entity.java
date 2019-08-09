82fb176e12eb8b1ac1c54d80a28e05df4fbc9bb1
TheMode <themode@outlook.fr>
2019-08-09 04:07:46 +0200

    Entity instance

--- src/main/java/fr/minestom/api/entity/Entity.java
+++ src/main/java/fr/minestom/api/entity/Entity.java
@@ -1,9 +1,14 @@
 package fr.minestom.api.entity;
 
 import fr.minestom.api.data.DataContainer;
+import fr.minestom.api.instance.Instance;
 import fr.minestom.api.utils.Location;
 
 public interface Entity extends DataContainer {
+
+    Instance getInstance();
+
+    void setInstance(Instance instance);
 
     Location getLocation();
 
--- src/main/java/fr/minestom/api/entity/LivingEntity.java
+++ src/main/java/fr/minestom/api/entity/LivingEntity.java
@@ -3,13 +3,8 @@ package fr.minestom.api.entity;
 import fr.minestom.api.entity.callback.EntityDamageCallback;
 import fr.minestom.api.entity.callback.EntityDeathCallback;
 import fr.minestom.api.entity.callback.EntityHitCallback;
-import fr.minestom.api.instance.Instance;
 
 public interface LivingEntity extends Entity {
-
-    Instance getInstance();
-
-    void setInstance(Instance instance);
 
     // Callbacks
     void onDeathCallback(EntityDeathCallback deathCallback);
