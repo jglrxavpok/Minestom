5a1ae8d7b814e15ea46e660182669b3f9c27f080
jglrxavpok <jglrxavpok@gmail.com>
2020-04-28 00:09:14 +0200

    Added Data object to DamageType

--- src/main/java/net/minestom/server/entity/damage/DamageType.java
+++ src/main/java/net/minestom/server/entity/damage/DamageType.java
@@ -2,19 +2,23 @@ package net.minestom.server.entity.damage;
 
 import club.thectm.minecraft.text.TextBuilder;
 import club.thectm.minecraft.text.TextObject;
+import net.minestom.server.data.Data;
+import net.minestom.server.data.DataContainer;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.Player;
 
 /**
  * Represents a type of damage
  */
-public class DamageType {
+public class DamageType implements DataContainer  {
 
     public static final DamageType VOID = new DamageType("void");
     public static final DamageType GRAVITY = new DamageType("gravity");
     private final String identifier;
+    private Data data;
 
     public DamageType(String identifier) {
+        this.data = new Data();
         this.identifier = identifier;
     }
 
@@ -36,5 +40,15 @@ public class DamageType {
 
     public static DamageType fromProjectile(Entity shooter, Entity projectile) {
         return new EntityProjectileDamage(shooter,  projectile);
+    }
+
+    @Override
+    public Data getData() {
+        return data;
+    }
+
+    @Override
+    public void setData(Data data) {
+        this.data = data;
     }
 }
